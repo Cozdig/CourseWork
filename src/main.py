bf8467c14afacb0ca6b69ed87fec86b0e31ae857
Cozdig <tank-20@list.ru>
2025-04-03 00:59:04 +0300

    main add docstring

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from src.views import main_views
 
 
 def main():
+    """Главная функция. Запускает все остальные функции и выдает результат в json файлах."""
     date_string = "11.07.2020 23:11:24"
     main_views(date_string)
     df = read_xlsx(input_file="../data/operations.xlsx")
